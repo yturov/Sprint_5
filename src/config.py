79c62169cbf38e2dd918ecdb6e6ed3b3eabff582
Yuri Turov <turow.yura2013@yandex.ru>
2024-09-02 11:08:11 +0300

    Sprint 5 isparavlenia

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 class Config:
     URL = 'https://stellarburgers.nomoreparties.site/'
-    URL_REG = 'https://stellarburgers.nomoreparties.site/register'
-    URL_INPUT = 'https://stellarburgers.nomoreparties.site/login'
-    URL_PASS_RECOVERY = 'https://stellarburgers.nomoreparties.site/forgot-password'
-    URL_ACCOUNT_PROFILE = 'https://stellarburgers.nomoreparties.site/account/profile'
+    URL_REG = f'{URL}register'
+    URL_INPUT = f'{URL}login'
+    URL_PASS_RECOVERY = f'{URL}forgot-password'
+    URL_ACCOUNT_PROFILE = f'{URL}account/profile'
     RESOLUTION = 1920, 1290
     TIMEOUT = 15
--- tests/test_section_construktor.py
+++ tests/test_section_construktor.py
@@ -1,49 +1,39 @@
-from selenium.webdriver.support.expected_conditions import element_to_be_clickable, visibility_of_element_located
-
 from src.config import Config
 from src.locators import Locators
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from src.data import UserData
-import time
 
 
 class TestSectionConstruktor:
 
     def test_section_sous(self, driver):
-        driver.find_element(*Locators.INPUT_MAIN_BUTTON).click()
-        WebDriverWait(driver, 2).until(EC.url_to_be(Config.URL_INPUT))
-        driver.find_element(*Locators.EMAIL_FIELD).send_keys(UserData.EMAIL)
-        driver.find_element(*Locators.PASSWORD_FIELD).send_keys(UserData.PASSWORD)
-        driver.find_element(*Locators.INPUT_MAIN_BUTTON).click()
+        WebDriverWait(driver, 2).until(EC.url_to_be(Config.URL))
+        sous_section = driver.find_element(*Locators.SOUS_SECTION)
+        nachinki_section = driver.find_element(*Locators.NACHINKI_SECTION)
+        bulki_section = driver.find_element(*Locators.BULKI_SECTION)
+        sous_section.click()
+        assert "current" in sous_section.get_attribute("Class")
+        assert "current" not in nachinki_section.get_attribute("Class")
+        assert "current" not in bulki_section.get_attribute("Class")
+
+    def test_section_nachinki(self, driver):
+        WebDriverWait(driver, 2).until(EC.url_to_be(Config.URL))
+        sous_section = driver.find_element(*Locators.SOUS_SECTION)
+        bulki_section = driver.find_element(*Locators.BULKI_SECTION)
+        nachinki_section = driver.find_element(*Locators.NACHINKI_SECTION)
+        nachinki_section.click()
+        assert "current" in nachinki_section.get_attribute("Class")
+        assert "current" not in sous_section.get_attribute("Class")
+        assert "current" not in bulki_section.get_attribute("Class")
+
+    def test_section_bulki(self, driver):
         WebDriverWait(driver, 2).until(EC.url_to_be(Config.URL))
         sous_section = driver.find_element(*Locators.SOUS_SECTION)
         sous_section.click()
-        assert sous_section.text == "Соусы"
-        assert driver.current_url == Config.URL
-
-    def test_section_nachinki(self, driver):
-        driver.find_element(*Locators.INPUT_MAIN_BUTTON).click()
-        WebDriverWait(driver, 2).until(EC.url_to_be(Config.URL_INPUT))
-        driver.find_element(*Locators.EMAIL_FIELD).send_keys(UserData.EMAIL)
-        driver.find_element(*Locators.PASSWORD_FIELD).send_keys(UserData.PASSWORD)
-        driver.find_element(*Locators.INPUT_MAIN_BUTTON).click()
-        WebDriverWait(driver, 2).until(EC.url_to_be(Config.URL))
         nachinki_section = driver.find_element(*Locators.NACHINKI_SECTION)
         nachinki_section.click()
-        assert nachinki_section.text == "Начинки"
-        assert driver.current_url == Config.URL
-
-    def test_section_bulki(self, driver):
-        driver.find_element(*Locators.INPUT_MAIN_BUTTON).click()
-        WebDriverWait(driver, 2).until(EC.url_to_be(Config.URL_INPUT))
-        driver.find_element(*Locators.EMAIL_FIELD).send_keys(UserData.EMAIL)
-        driver.find_element(*Locators.PASSWORD_FIELD).send_keys(UserData.PASSWORD)
-        driver.find_element(*Locators.INPUT_MAIN_BUTTON).click()
-        WebDriverWait(driver, 2).until(EC.url_to_be(Config.URL))
-        driver.find_element(*Locators.NACHINKI_SECTION).click()
-        driver.find_element(*Locators.SOUS_SECTION).click()
         bulki_section = driver.find_element(*Locators.BULKI_SECTION)
         bulki_section.click()
-        assert bulki_section.text == "Булки"
-        assert driver.current_url == Config.URL
+        assert "current" in bulki_section.get_attribute("Class")
+        assert "current" not in sous_section.get_attribute("Class")
+        assert "current" not in nachinki_section.get_attribute("Class")
